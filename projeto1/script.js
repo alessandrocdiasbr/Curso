f892c40a4d804959d6af5245783e36d110eea514
Alessandro <158523798+alessandrodias83@users.noreply.github.com>
2024-09-11 16:12:16 -0300

    atualizei atividade

--- projeto1/script.js
+++ projeto1/script.js
@@ -1,5 +1,10 @@
 
 function perguntarIdade() {
-    prompt('Qual o ano de nascimento?');
+
+    /* Só uso o let/const uma vez, se precisar declarar novamente a variavel, posso dizer somente o nome da variável */
+
+    const anoNascimento = prompt('Qual o ano de nascimento?'); 
+    alert("Você nasceu em " + anoNascimento);
+
 }
 
